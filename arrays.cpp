60a3b66a4efc580cea9747cda32a511364e43f90
ellie <74204751+Ms-elliebb@users.noreply.github.com>
2024-02-10 04:22:20 +0300

    Create arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int n;
+   cin>>n;
+   int numbers[n];
+   
+   for (int i=0; i<n;i++) {
+   cin>>numbers[i];
+   }
+   
+   for (int i=n-1;i>=0 ; i--) {
+   cout<<numbers[i]<<" ";
+   }
+    
+    return 0;
+}
