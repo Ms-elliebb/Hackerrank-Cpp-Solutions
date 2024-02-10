03cfd77e2ff89d16ff30ce68e603da09f96b5b73
ellie <74204751+Ms-elliebb@users.noreply.github.com>
2024-02-10 04:16:28 +0300

    Create Basic data types

---
+++ Basic data types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    
+    int e; long f; char c; float a; double d;
+    
+    cin>>e>>f>>c>>a>>d;
+    
+    printf("%d \n ",e);
+    printf("%ld \n",f);
+    printf("%c \n",c);
+    printf("%f \n",a);
+    printf("%lf \n",d);
+    
+    
+    return 0;
+} 
