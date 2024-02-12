7a9d6e575c54ea5a89735d4e2a4214b240784010
ellie <74204751+Ms-elliebb@users.noreply.github.com>
2024-02-12 21:19:44 +0300

    Create vector-erase.cpp

---
+++ vector-erase.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    vector<int> v;
+    int n,a,b,c,d;
+    cin>>n;
+    for (int i=0; i<n; i++) {
+        cin>>a;
+        v.push_back(a);
+    }
+    cin>>b;
+   v.erase(v.begin()+b);
+   cin>>c>>d;
+   v.erase(v.begin()+(c-1),v.begin()+(d-1));
+   
+   cout<<v.size()<<endl;
+   
+   for (int num: v) {
+   cout<<num<<" ";
+   }
+   
+    
+    return 0;
+}
